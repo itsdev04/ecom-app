42f4c1b188723a30b5b8963899974f32128634e7
itsdev04 <imkrdev@gmail.com>
2025-08-05 09:39:11 +0530

    product list with API

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-seller-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './seller-home.component.html',
   styleUrl: './seller-home.component.css'
 })
 export class SellerHomeComponent {
+  productList: undefined | product[];
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.productService.productList().subscribe((data) => {
+      console.log(data);
+      this.productList = data;
+    });
+  }
 }
