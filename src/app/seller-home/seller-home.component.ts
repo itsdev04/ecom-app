8bbcb3e2e5005d58e97a468a141dd55806ac45c6
itsdev04 <imkrdev@gmail.com>
2025-08-06 10:54:58 +0530

    add fontawesome icon

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { product } from '../data-type';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-seller-home',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './seller-home.component.html',
   styleUrl: './seller-home.component.css'
 })
 export class SellerHomeComponent {
   productList: undefined | product[];
   productMessage: string = '';
+  icon = faTrash;
 
   constructor(private productService: ProductService) { }
 
